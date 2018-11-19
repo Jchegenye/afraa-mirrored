431942705eb419a63e03cf4d0b730ff16ebd0be2
ngarawakimani <dancankimani70@gmail.com>
2018-11-20 00:51:14 +0300

    sessions bug fix

--- app/ProgrammeSession.php
+++ app/ProgrammeSession.php
@@ -10,16 +10,28 @@ class ProgrammeSession extends Model
     //
     public function getSessions(){
 
+        // $sessions = DB::table('programme_sessions')
+        //     ->join('users', 'users.uid', '=', 'programme_sessions.user_id')
+        //     ->join('profiles', 'profiles.user_id', '=', 'users.uid')
+        //     ->where('users.uid=programme_sessions.user_id')
+        //     ->get();
+
         $sessions = DB::table('programme_sessions')
-                ->join('users', function ($join) {
+            ->join('users', 'users.uid', '=', 'programme_sessions.user_id')
+            ->join('profiles', 'profiles.user_id', '=', 'users.uid')
 
-                    $join->on('programme_sessions.id', '=', 'users.uid');
-                })
-                ->join('profiles', function ($join) {
+            ->get();
 
-                    $join->on('profiles.user_id', '=', 'users.uid');
-                })
-                ->get();
+        // $sessions = DB::table('programme_sessions')
+        //         ->join('users', function ($join) {
+
+        //             $join->on('programme_sessions.id', '=', 'users.uid');
+        //         })
+        //         ->join('profiles', function ($join) {
+
+        //             $join->on('profiles.user_id', '=', 'users.uid');
+        //         })
+        //         ->get();
 
         return $sessions;
     }
